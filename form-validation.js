ee4b5ecab0366ca4d5525e2c289ab86d6bb2d0c8
SaveAmericaAgain <142932822+SaveAmericaAgain@users.noreply.github.com>
2023-09-18 01:02:03 +0100

    Add files via upload

---
+++ form-validation.js
@@ -0,0 +1,34 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+var formchecks = document.getElementsByClassName("form-check");
+var tabcontents = document.getElementsByClassName("tab-contents")
+
+function opentab(tabname){
+  for(formcheck of formchecks){
+    formcheck.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
